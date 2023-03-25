fe6c93ac54c8b1143d393f248cb402f2f7651964
JF Git 1 <juliofarias+git1@gmail.com>
2023-03-25 00:04:33 -0300

    Tailwind version 0.2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 
-/* import { HeaderWrapper } from './styles.js'; */
-
 export function Header() {
 	return (
-		<div className='fixed top-0 left-0 z-50 w-full bg-slate-800'>
-			<header className='container mx-auto flex items-center justify-between py-4'>
+		<div className='fixed top-0 left-0 z-50 w-full bg-slate-800 px-4 '>
+			<header className='container mx-auto flex flex-col items-center py-4 md:flex-row md:justify-between'>
 				<h1 className='text-5xl font-bold leading-none'>
 					<Link className='text-sky-500' href='/' scroll={false} prefetch={false}>
 						AERO
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,19 @@ import Seo from '@/components/Seo';
 import { Footer } from '@/components/Footer';
 import LayoutMotion from '@/components/LayoutMotion';
 
+import Image from 'next/image';
+
 export default function Home() {
 	console.log('load home');
 	return (
-		<div className='pt-28'>
+		<div className='px-4 pt-28 '>
 			<LayoutMotion>
 				<Seo title='Aero Design' desc='Content of the homepage.' />
 				<main className='container mx-auto'>
 					<h1 className='mb-4 text-3xl'>Welcome</h1>
 					<div>
 						<figure className='overflow-hidden rounded-xl bg-slate-100 p-8 dark:bg-slate-800 md:flex md:p-0'>
-							<img className='mx-auto h-24 w-24 rounded-full md:h-auto md:w-48 md:rounded-none ' src='/images/image-01.jpg' alt='' width='384' height='512' />
+							<Image className='mx-auto h-24 w-24 rounded-full md:h-auto md:w-48 md:rounded-none ' src='/images/image-01.jpg' alt='' width='384' height='512' />
 							<div className='space-y-4 pt-6 text-center md:p-8 md:text-left'>
 								<blockquote>
 									<p className='text-lg font-light'>
